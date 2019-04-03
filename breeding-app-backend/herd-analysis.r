84fbf2115b72ce0a82a71a5743f2cd9d90dafc37
clive-h-townsend <clive.h.townsend@gmail.com>
2019-04-03 01:04:56 -0400

    Full API Prediction

--- breeding-app-backend/herd-analysis.r
+++ breeding-app-backend/herd-analysis.r
@@ -1,10 +1,10 @@
 library(ggplot2)
-#setwd('/home/clive/Desktop/GHS/breeding-app/breeding-app-backend/TestData')
-setwd('~/Desktop/Githubs/breeding-app-2/breeding-app-backend/SourceData/')
+setwd('/home/clive/Desktop/GHS/breeding-app/breeding-app-backend/')
+#setwd('~/Desktop/Githubs/breeding-app/breeding-app-backend/SourceData/')
 print(getwd())
 
-#df <- read.csv(file="CleanData.csv")
-df <- read.csv(file="AllData.csv")
+df <- read.csv(file="CleanData.csv")
+#df <- read.csv(file="allData.csv")
 
 df$BirthYear <- with(df, format(as.Date(BirthDate, format="%Y-%m-%d"), "%Y"))
 
@@ -29,7 +29,6 @@ for (epd in epds) {
   
   plot <- ggplot(dfepds, aes(years, epdTrend))
   plot <- plot + geom_point(color='blue', size=5, shape=21, fill="white", stroke=5) #+ stat_smooth(method="lm", col="red")
-  #plot <- plot + geom_bar(stat="identity")
   plot <- plot + theme_classic()
   plot <- plot + ggtitle(paste("Year vs. ",epdNames[i]))
   plot <- plot + xlab("Year")
@@ -70,40 +69,42 @@ simpledf <- data.frame(
   "TI" = df$Epds.Ti
 )
 
-#print(cor(simpledf, use="complete.obs", method="pearson"))
 
-fitBW <- lm(API ~ BW, data=simpledf, na.action=na.omit)
-#fitCE <- lm(API ~ CE, data=simpledf, na.action=na.omit)
-fitMarb <- lm(API ~ Marb, data=simpledf, na.action=na.omit)
-fitStay <- lm(API ~ Stay, data=simpledf, na.action=na.omit)
+apiFullPrediction <- lm(API~ CE + BW + MCE + WW + YW + Marb + YG + BF + REA + SHR + Stay + Milk + MWW + CW + Doc + ADG, data = simpledf, na.action=na.omit)
+apiFullPredictionNoShr <- lm(API~ CE + BW + MCE + WW + YW + Marb + YG + BF + REA + Stay + Milk + MWW + CW + Doc + ADG, data = simpledf, na.action=na.omit)
+apiSimplePrediction <- lm(API~ CE + BW + WW + YW + Marb + YG + BF + Stay + ADG, data = simpledf, na.action=na.omit)
+tiFullPrediction <- lm(TI~ CE + BW + MCE + WW + YW + Marb + YG + BF + REA + SHR + Stay + Milk + MWW + CW + Doc + ADG, data = simpledf, na.action=na.omit)
+tiSimplePrediction <- lm(TI~ BW + WW + Marb, data = simpledf, na.action=na.omit)
 
-BWAPIPred <- simpledf$BW*fitBW$coefficients[2] + fitBW$coefficients[1]
-#CEAPIPred <- simpledf$CE*fitCE$coefficients[2] + fitCE$coefficients[1]
-MarbAPIPred <- simpledf$Marb*fitMarb$coefficients[2] + fitCE$coefficients[1]
-StayAPIPred <- simpledf$Stay*fitStay$coefficients[2] + fitStay$coefficients[1]
+#print('full api')
+#print(summary(apiFullPrediction))
+print('full api (no shr)')
+print(summary(apiFullPredictionNoShr))
+#print('simple api')
+#print(summary(apiSimplePrediction))
+#print('full ti')
+#print(summary(tiFullPrediction))
+#print('simple ti')
+#print(summary(tiSimplePrediction))
 
-BWWeight <- 1.0
-#CEWeight <- 1.1
-MarbWeight <- 2.1
-StayWeight <- 1.7
+apiFPCoef <- summary(apiFullPrediction)$coefficients
+apiFPNoShrCoef <- summary(apiFullPredictionNoShr)$coefficients
+apiSPCoef <- summary(apiSimplePrediction)$coefficients
+tiFPCoef <- summary(tiFullPrediction)$coefficients
+tiSPCoef <- summary(tiSimplePrediction)$coefficients
 
-#WeightSum <- BWWeight + CEWeight + MarbWeight + StayWeight
-WeightSum <- BWWeight + MarbWeight + StayWeight
+simpledf$calculatedFPAPI <- apiFPCoef[1] + (apiFPCoef[2]*simpledf$CE) + (apiFPCoef[3]*simpledf$BW) + (apiFPCoef[4]*simpledf$MCE) + (apiFPCoef[5]*simpledf$WW) + (apiFPCoef[6]*simpledf$YW) + (apiFPCoef[7]*simpledf$Marb) + (apiFPCoef[8]*simpledf$YG) + (apiFPCoef[9]*simpledf$BF) + (apiFPCoef[10]*simpledf$REA) + (apiFPCoef[11]*simpledf$SHR) + (apiFPCoef[12]*simpledf$Stay) + (apiFPCoef[13]*simpledf$Milk) + (apiFPCoef[14]*simpledf$MWW) + (apiFPCoef[15]*simpledf$CW) + (apiFPCoef[16]*simpledf$Doc) + (apiFPCoef[17]*simpledf$ADG)
+simpledf$calculatedFPAPINoShr <- apiFPNoShrCoef[1] + (apiFPNoShrCoef[2]*simpledf$CE) + (apiFPNoShrCoef[3]*simpledf$BW) + (apiFPNoShrCoef[4]*simpledf$MCE) + (apiFPNoShrCoef[5]*simpledf$WW) + (apiFPNoShrCoef[6]*simpledf$YW) + (apiFPNoShrCoef[7]*simpledf$Marb) + (apiFPNoShrCoef[8]*simpledf$YG) + (apiFPNoShrCoef[9]*simpledf$BF) + (apiFPNoShrCoef[10]*simpledf$REA) + (apiFPNoShrCoef[11]*simpledf$Stay) + (apiFPNoShrCoef[12]*simpledf$Milk) + (apiFPNoShrCoef[13]*simpledf$MWW) + (apiFPNoShrCoef[14]*simpledf$CW) + (apiFPNoShrCoef[15]*simpledf$Doc) + (apiFPNoShrCoef[16]*simpledf$ADG)
+simpledf$calculatedSPAPI <- apiSPCoef[1] + (apiSPCoef[2]*simpledf$CE) + (apiSPCoef[3]*simpledf$Marb) + (apiSPCoef[4]*simpledf$Stay)+ (apiSPCoef[5]*simpledf$SHR)
 
-#meanAPIPred <- ((BWAPIPred * BWWeight) + (CEAPIPred*CEWeight) + (MarbAPIPred*MarbWeight) + (StayAPIPred*StayWeight)) / WeightSum
-meanAPIPred <- ((BWAPIPred * BWWeight) + (MarbAPIPred*MarbWeight) + (StayAPIPred*StayWeight)) / WeightSum
 
-simpledf$APIPredictor <- meanAPIPred
-
-plot <- ggplot(simpledf, aes(x=meanAPIPred, y=API))
-plot <- plot + geom_point(color='blue') #+ stat_smooth(method="lm", col="red")
+#plot <- ggplot(simpledf, aes(x=calculatedFPAPINoShr, y=API))
+plot <- ggplot(simpledf, aes(x=calculatedFPAPI, y=API))
+#plot <- ggplot(simpledf, aes(x=calculatedSPAPI, y=API))
+plot <- plot + geom_point(color='blue')
 plot <- plot + theme_classic()
-plot <- plot + ggtitle("API Prediction vs. Actual")
+plot <- plot + ggtitle("API Prediction (No SHR) vs. Actual")
 plot <- plot + xlab("API Prediction")
-plot <- plot + xlim(c(75, 175))
-plot <- plot + ylim(c(75, 175))
-
 print(plot)
 
-print(cor(simpledf$API, simpledf$APIPredictor, use="complete.obs", method="pearson"))
 
